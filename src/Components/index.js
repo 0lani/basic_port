18feb8a810c5c60ca3aa79521cae2ad5bd784447
boredasfawk <olonnyetaylor@gmail.com>
2020-08-21 16:47:45 -0700

    removed debugger

--- src/Components/index.js
+++ src/Components/index.js
@@ -2,15 +2,15 @@ import React, { lazy, Suspense } from 'react';
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import { Spin, Result } from 'antd';
 
-//const LandingComponent = lazy(() => import('./HomePageLayout'));
+const LandingComponent = lazy(() => import('./HomePageLayout'));
 const ProjectsComponent = lazy(() => import('./SitePagesLayout'));
 const AboutComponent = lazy(() => import('./AboutPagesLayout'));
 
-const debug = lazy(async () => {
-  return new Promise(resolve => setTimeout(resolve, 10000)).then(
-    () => import("./HomePageLayout")
-  );
-})
+// const debug = lazy(async () => {
+//   return new Promise(resolve => setTimeout(resolve, 10000)).then(
+//     () => import("./HomePageLayout")
+//   );
+// })
 
 const renderLoading = () => (
   <div id="loadingContainer">
@@ -32,7 +32,7 @@ const App = () => {
     <BrowserRouter>
       <Switch>
         <Suspense fallback={renderLoading()}>
-          <Route exact path="/" component={debug}/>
+          <Route exact path="/" component={LandingComponent}/>
           <Route path="/projects" component={ProjectsComponent}/>
           <Route path="/about" component={AboutComponent}/>
         </Suspense>
