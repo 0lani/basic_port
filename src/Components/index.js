19b09170f7a6316ff6c6a0f726d97b18bac54040
boredasfawk <olonnyetaylor@gmail.com>
2020-08-21 18:47:48 -0700

    fixed loading screen by dividing suspense up

--- src/Components/index.js
+++ src/Components/index.js
@@ -2,16 +2,6 @@ import React, { lazy, Suspense } from 'react';
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import { Spin, Result } from 'antd';
 
-const LandingComponent = lazy(() => import('./HomePageLayout'));
-const ProjectsComponent = lazy(() => import('./SitePagesLayout'));
-const AboutComponent = lazy(() => import('./AboutPagesLayout'));
-
-// const debug = lazy(async () => {
-//   return new Promise(resolve => setTimeout(resolve, 10000)).then(
-//     () => import("./HomePageLayout")
-//   );
-// })
-
 const renderLoading = () => (
   <div id="loadingContainer">
     <div id="loading">
@@ -24,6 +14,10 @@ const renderLoading = () => (
   </div>
 );
 
+const LandingComponent = lazy(() => import('./HomePageLayout'));
+const ProjectsComponent = lazy(() => import('./SitePagesLayout'));
+const AboutComponent = lazy(() => import('./AboutPagesLayout'));
+
 //TODO: make errorboundry for loading and a 404 page 
 
 const App = () => {
@@ -31,15 +25,33 @@ const App = () => {
   return (
     <BrowserRouter>
       <Switch>
-        <Suspense fallback={renderLoading()}>
-          <Route exact path="/" component={LandingComponent}/>
-          <Route path="/projects" component={ProjectsComponent}/>
-          <Route path="/about" component={AboutComponent}/>
-        </Suspense>
+        <Route exact path="/" render={props => (
+          <Suspense fallback={renderLoading()}>
+           <LandingComponent {...props}/>
+         </Suspense>
+        )}/>
+
+        <Route path="/projects" render={props => (
+          <Suspense fallback={renderLoading()}>
+           <ProjectsComponent {...props}/>
+         </Suspense>
+        )}/>
+
+        <Route path="/about" render={props => (
+          <Suspense fallback={renderLoading()}>
+           <AboutComponent {...props}/>
+         </Suspense>
+        )}/>
       </Switch>
     </BrowserRouter>
   )
-
 }
 
 export default App;
+
+
+// const debug = lazy(async () => {
+//   return new Promise(resolve => setTimeout(resolve, 10000)).then(
+//     () => import("./HomePageLayout")
+//   );
+// })
